b97bfee87b746280550187b8a3545de33f1e7bdb
Наталия <natalia@MacBook-Air-Natalia.local>
2015-04-02 17:12:59 +0300

    Initial commit

---
+++ Homework5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework5
+//
+//  Created by Наталия on 02.04.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Homework5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Homework5
+//
+//  Created by Наталия on 02.04.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
