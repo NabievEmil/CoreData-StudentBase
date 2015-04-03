8f735c46126797ab1c77dad6255ffa48e3a524ae
Наталия <natalia@MacBook-Air-Natalia.local>
2015-04-03 22:34:18 +0300

    Base filled

--- Homework5/AppDelegate.m
+++ Homework5/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "University.h"
+#import "Student.h"
+#import "Facultative.h"
 
 @interface AppDelegate ()
 
@@ -15,8 +18,57 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    NSManagedObjectModel *model = [NSManagedObjectModel mergedModelFromBundles:nil];
+    
+    self.managedObjectModel = model;
+
+    NSURL *docsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask]lastObject];
+    
+    NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+    
+    [coordinator addPersistentStoreWithType:NSSQLiteStoreType
+                              configuration:nil
+                                        URL:[docsURL URLByAppendingPathComponent:@"Homework5.sqlite"]
+                                    options:nil
+                                      error:nil];
+    self.persistentStoreCoordinator = coordinator;
+    self.managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    self.managedObjectContext.persistentStoreCoordinator = coordinator;
+    
+//    for (int i = 0; i < 3; i++)
+//    {
+//        University *university = [NSEntityDescription insertNewObjectForEntityForName:@"University"
+//                                                                inManagedObjectContext:self.managedObjectContext];
+//        university.universityName = [NSString stringWithFormat:@"University %d",i+1];
+//        for (int j = 0; j < 3; j++)
+//        {
+//            Facultative *facultative = [NSEntityDescription insertNewObjectForEntityForName:@"Facultative" inManagedObjectContext:self.managedObjectContext];
+//            facultative.facultativeName = [NSString stringWithFormat:@"Facultative %d.%d",i+1,j+1];
+//            facultative.university = university;
+//            for (int k = 0; k < 3; k++)
+//            {
+//                Student *student = [NSEntityDescription insertNewObjectForEntityForName:@"Student" inManagedObjectContext:self.managedObjectContext];
+//                student.firstName = [NSString stringWithFormat:@"Student %d.%d.%d",i+1,j+1,k+1];
+//                student.facultative = facultative;
+//                student.course = [[NSNumber alloc]initWithInt:arc4random() % 4 + 1];
+//                [facultative addStudentsObject:student];
+//            }
+//            [university addFacultativesObject:facultative];
+//        }
+//        
+//    }
+    
+//    [self.managedObjectContext save:nil];
+    
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"University"];
+    
+    NSArray *universities = [self.managedObjectContext executeFetchRequest:fetchRequest
+                                                                   error:nil];
+    NSLog(@"%@",universities);
+    
+
     return YES;
 }
 
@@ -44,84 +96,5 @@
     [self saveContext];
 }
 
-#pragma mark - Core Data stack
-
-@synthesize managedObjectContext = _managedObjectContext;
-@synthesize managedObjectModel = _managedObjectModel;
-@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
-
-- (NSURL *)applicationDocumentsDirectory {
-    // The directory the application uses to store the Core Data store file. This code uses a directory named "-----.Homework5" in the application's documents directory.
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-}
-
-- (NSManagedObjectModel *)managedObjectModel {
-    // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
-    if (_managedObjectModel != nil) {
-        return _managedObjectModel;
-    }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Homework5" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}
-
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
-    // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it.
-    if (_persistentStoreCoordinator != nil) {
-        return _persistentStoreCoordinator;
-    }
-    
-    // Create the coordinator and store
-    
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Homework5.sqlite"];
-    NSError *error = nil;
-    NSString *failureReason = @"There was an error creating or loading the application's saved data.";
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-        // Report any error we got.
-        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-        dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
-        dict[NSLocalizedFailureReasonErrorKey] = failureReason;
-        dict[NSUnderlyingErrorKey] = error;
-        error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
-        // Replace this with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }
-    
-    return _persistentStoreCoordinator;
-}
-
-
-- (NSManagedObjectContext *)managedObjectContext {
-    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
-    if (_managedObjectContext != nil) {
-        return _managedObjectContext;
-    }
-    
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-    if (!coordinator) {
-        return nil;
-    }
-    _managedObjectContext = [[NSManagedObjectContext alloc] init];
-    [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-    return _managedObjectContext;
-}
-
-#pragma mark - Core Data Saving support
-
-- (void)saveContext {
-    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-    if (managedObjectContext != nil) {
-        NSError *error = nil;
-        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
-        }
-    }
-}
 
 @end
---
+++ Homework5/StudentBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudentBaseViewController.h
+//  Homework5
+//
+//  Created by Наталия on 03.04.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentBaseViewController : UITableViewController
+
+@end
