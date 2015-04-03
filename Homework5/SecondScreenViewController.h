e853ca0b2336d080a7939ecd2467566475312cd1
Наталия <natalia@MacBook-Air-Natalia.local>
2015-04-03 23:28:09 +0300

    Second screen doesn't work

---
+++ Homework5/SecondScreenViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondScreenViewController.h
+//  Homework5
+//
+//  Created by Наталия on 03.04.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondScreenViewController : UITableViewController
+
+@property (strong,nonatomic) NSArray *facultatives;
+
+@end
